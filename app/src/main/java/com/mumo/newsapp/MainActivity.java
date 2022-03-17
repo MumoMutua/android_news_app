e1629fdbda7c8a86f2932af3b1f40eede70634eb
Mumo Mutua <mumo.related@gmail.com>
2022-03-17 10:05:20 +0300

    Added animation GIF to Splash Screen

--- app/src/main/java/com/mumo/newsapp/MainActivity.java
+++ app/src/main/java/com/mumo/newsapp/MainActivity.java
@@ -2,13 +2,30 @@ package com.mumo.newsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+
+    Handler delayHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView image = findViewById(R.id.img_mine);
+        Glide.with(this).load(R.raw.animation2).into(image);
+
+        delayHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+               startActivity(new Intent(MainActivity.this, HomeActivity.class));
+            }
+        }, 6000);
     }
 }
